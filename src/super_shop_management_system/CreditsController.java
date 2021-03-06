8068d43a41c6b544e8ac5756078b98a954953653
Ali Akber <77451638+AliAkber123@users.noreply.github.com>
2021-03-06 20:59:06 +0600

    Add_1
    Let's see

--- src/super_shop_management_system/CreditsController.java
+++ src/super_shop_management_system/CreditsController.java
@@ -5,9 +5,18 @@
  */
 package super_shop_management_system;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,6 +25,9 @@ import javafx.fxml.Initializable;
  */
 public class CreditsController implements Initializable {
 
+    @FXML
+    private Button Back_btn;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +35,14 @@ public class CreditsController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void Back_btn_Action(ActionEvent event)throws IOException {
+Parent After_Login = FXMLLoader.load(getClass().getResource("After_Login.fxml"));
+        Scene src5 = new Scene (After_Login);
+        Stage app_primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_primaryStage.setScene(src5);
+        app_primaryStage.show();
+    }
     
 }
